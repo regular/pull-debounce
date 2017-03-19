57881f1e264306a62dd663bed07b410c86ee01b0
Jan Bölsche <regular.gonzales@googlemail.com>
2017-03-19 17:01:10 +0100

    All tests pass

--- index.js
+++ index.js
@@ -5,41 +5,64 @@ module.exports = function debounce (ms) {
   let start = Date.now();
   let queue = [];
   let ended = false;
+  let readPending = false;
+  let candidate = null;
+
+  function callback(end, data) {
+    console.log('callback q', queue.length);
+    if (queue.length) {
+      queue.shift()(end, data);
+    }
+  }
 
   return read => {
     let timeoutWon = false;
     return function next (end, cb) {
-      if (end) return read(end, cb);
-      ended = ended || end;
-      if (ended) return cb(end);
-      if (queue.length) {
-        let result = queue.shift();
-        console.log('unqueue', result);
-        return cb(result[0], result[1]);
+      console.log('next called');
+      if (cb) {
+        console.log('queueing callback');
+        queue.push(cb);
       }
+      if (ended) return callback(ended);
+      if (readPending) {
+        console.log('read pending. returning');
+        return;
+      }
+      if (end) return read(end, callback);
+      ended = ended || end;
+      console.log('calling read');
+      readPending = true;
       read(end, function (end, data) {
+        readPending = false;
+        console.log('read done, new candidate:', end, data);
+        if (end) {
+          // always emit last item
+          ended = end;
+          return callback(candidate[0], candidate[1]);
+        }
         let t = Date.now() - start;
+        candidate = [end, data];
         if (timeoutWon) {
           timeoutWon = false;
-          console.log('timeout won, queueing', data);
-          return queue.push([end, data]);
+          console.log('timeout won');
+          if (queue.length === 0) return;
         }
         console.log('Cleaning timout');
         clearTimeout(timeout);
         if (end) {
           console.log(t, data, 'calling cb because end');
-          return cb(end, data);
+          return callback(end, data);
         }
 
         timeout = setTimeout(() => {
           let t = Date.now() - start;
           console.log(t, data, 'calling cb (timeout)');
           timeoutWon = true;
-          cb(end, data);
+          callback(candidate[0], candidate[1]);
         }, ms);
 
-        console.log('setTimeout after', ms, 'for', data);
-        return next(end, cb);
+        console.log('setting Timeout after', ms, 'for', data, 'calling next');
+        return next(end);
       });
     };
   };
